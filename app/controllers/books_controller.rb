d0747a6c617b79c6066a2277f6294c8a9a4eb59b
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-14 12:25:41 +0000

    [Add] プロフィール編集画面作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,8 +11,14 @@ class BooksController < ApplicationController
     redirect_to book_path(@book)
   end
   def edit
+    @book = Book.find(params[:id])
   end
-
+  def update
+   book = Book.find(params[:id])
+   book.user_id = current_user.id
+   book.update(book_params)
+   redirect_to book_path(book.id)
+  end
   def show
     @book = Book.find(params[:id])
     @user =@book.user
