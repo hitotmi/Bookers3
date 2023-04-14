c56f77e4c39db27aa426fdb513d551442b68155d
hitotmi <129574187+hitotmi@users.noreply.github.com>
2023-04-14 09:40:54 +0000

    [Add] bootstrapinstall

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/edit'
+  get 'books/show'
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
